f027df4ce291098e68331fc7d6a0279fbc937a5a
Alex <alexey.gerasev@gmail.com>
2018-10-16 14:20:25 +0700

    test scope

--- iocBoot/iocLXI/st.cmd
+++ iocBoot/iocLXI/st.cmd
@@ -21,11 +21,11 @@ LXI_registerRecordDeviceDriver pdbbase
 
 # Set up ASYN ports
 
-drvAsynIPPortConfigure("L0","10.0.0.11:5025",0,0,0)
-asynOctetSetInputEos("L0", -1, "\n")
-asynOctetSetOutputEos("L0", -1, "\n")
-asynSetTraceIOMask("L0",-1,0x2)
-asynSetTraceMask("L0",-1,0x9)
+#drvAsynIPPortConfigure("L0","10.0.0.11:5025",0,0,0)
+#asynOctetSetInputEos("L0", -1, "\n")
+#asynOctetSetOutputEos("L0", -1, "\n")
+#asynSetTraceIOMask("L0",-1,0x2)
+#asynSetTraceMask("L0",-1,0x9)
 
 drvAsynIPPortConfigure("L1","10.0.0.12:5025",0,0,0)
 asynOctetSetInputEos("L1", -1, "\n")
@@ -35,7 +35,7 @@ asynSetTraceMask("L1",-1,0x9)
 
 ###############################################################################
 ## Load record instances
-dbLoadRecords("db/devFC.db","P=$(P),R=$(RF),PORT=L0,A=0")
+#dbLoadRecords("db/devFC.db","P=$(P),R=$(RF),PORT=L0,A=0")
 dbLoadRecords("db/devScope.db","P=$(P),R=$(RS),PORT=L1,A=0")
 
 ############################################################################### 
