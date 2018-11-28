3124fbdac0c7981ee9058bb4771fba70493f34be
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-11-28 17:37:31 +0700

    deploy settings

--- iocBoot/iocLXI/st.cmd
+++ iocBoot/iocLXI/st.cmd
@@ -21,22 +21,29 @@ LXI_registerRecordDeviceDriver pdbbase
 
 # Set up ASYN ports
 
-drvAsynIPPortConfigure("L0","10.0.0.11:5025",0,0,0)
-asynOctetSetInputEos("L0", -1, "\n")
-asynOctetSetOutputEos("L0", -1, "\n")
-asynSetTraceIOMask("L0",-1,0x2)
-asynSetTraceMask("L0",-1,0x9)
+drvAsynIPPortConfigure("P_V3FC","freq-counter-v3:5025",0,0,0)
+asynOctetSetInputEos("P_V3FC", -1, "\n")
+asynOctetSetOutputEos("P_V3FC", -1, "\n")
+asynSetTraceIOMask("P_V3FC",-1,0x2)
+asynSetTraceMask("P_V3FC",-1,0x9)
 
-drvAsynIPPortConfigure("L1","10.0.0.12:5025",0,0,0)
-asynOctetSetInputEos("L1", -1, "\n")
-asynOctetSetOutputEos("L1", -1, "\n")
-asynSetTraceIOMask("L1",-1,0x2)
-asynSetTraceMask("L1",-1,0x9)
+drvAsynIPPortConfigure("P_V4FC","freq-counter-v4:5025",0,0,0)
+asynOctetSetInputEos("P_V4FC", -1, "\n")
+asynOctetSetOutputEos("P_V4FC", -1, "\n")
+asynSetTraceIOMask("P_V4FC",-1,0x2)
+asynSetTraceMask("P_V4FC",-1,0x9)
+
+#drvAsynIPPortConfigure("L1","10.0.0.12:5025",0,0,0)
+#asynOctetSetInputEos("L1", -1, "\n")
+#asynOctetSetOutputEos("L1", -1, "\n")
+#asynSetTraceIOMask("L1",-1,0x2)
+#asynSetTraceMask("L1",-1,0x9)
 
 ###############################################################################
 ## Load record instances
-dbLoadRecords("db/devFC.db","P=$(P),R=$(RF),PORT=L0,A=0")
-dbLoadRecords("db/devScope.db","P=$(P),R=$(RS),PORT=L1,A=0")
+dbLoadRecords("db/devFC.db","P=VEPP3_,R=FREQ_,PORT=P_V3FC,A=0")
+dbLoadRecords("db/devFC.db","P=VEPP4_,R=FREQ_,PORT=P_V4FC,A=0")
+#dbLoadRecords("db/devScope.db","P=$(P),R=$(RS),PORT=L1,A=0")
 
 ############################################################################### 
 ## Start EPICS
